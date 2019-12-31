43adc9798fd4783a65920a6deb143af1da5b386a
kaleb <kaleb.lucas@aluno.ufabc.edu.br>
2019-12-31 15:08:51 -0300

    pendencias de transações

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,19 +1,19 @@
 function Blockchain() {
     this.chain = [];
-    this.newTransactions = [];
+    this.pendingTransactions = [];
 }
 
 Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash,hash) {
     const newBlock = {
         index: this.chain.length + 1,
         timestamp: Date.now(),
-        transactions: this.newTransactions,
+        transactions: this.pendingTransactions,
         nonce: nonce, 
         hash: hash,
         previousBlockHash: previousBlockHash
     };
 
-    this.newTransactions = [];
+    this.pendingTransactions = [];
     this.chain.push(newBlock);
 
     return newBlock;
@@ -22,6 +22,18 @@ Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash,hash) {
 
 Blockchain.prototype.getLastBlock = function() {
     return this.chain[this.chain.length - 1];
+}
+
+Blockchain.prototype.createNewTransaction = function(amount, sender, recipient){
+    const newTransaction = {
+        amount: amount,
+        sender: sender,
+        recipient: recipient
+    };
+
+    this.pendingTransactions.push(newTransaction);
+
+    return this.getLastBlock()['index'] + 1;
 }
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -3,7 +3,14 @@ const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain;
 
 bitcoin.createNewBlock(2349,'aydya78dga87dg8ads','1y87137y173y11913');
-bitcoin.createNewBlock(111,'AHUDASUDHAUSHD','A9S89YSAHDAH');
-bitcoin.createNewBlock(2899,'HAUDHASDHA8888A','U8UZ89UZY89YWY');
+bitcoin.createNewTransaction(100,'a87sdh87agd8a','adh8289eqh9a0');
+
+bitcoin.createNewBlock(234,'ksisooskoksoks','ah8dha8sdha8hd8a');
+
+bitcoin.createNewTransaction(5,'a87sdh87agd8a','adh8289eqh9a0');
+bitcoin.createNewTransaction(101110,'a87sdh87agd8a','adh8289eqh9a0');
+bitcoin.createNewTransaction(1067770,'a87sdh87agd8a','adh8289eqh9a0');
+
+bitcoin.createNewBlock(56234,'k223891q9dass','affad8sd384981u291a');
 
 console.log(bitcoin);
