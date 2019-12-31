36b04fc5f48c0212ee32afd56e521311f8b963a3
kaleb <kaleb.lucas@aluno.ufabc.edu.br>
2019-12-31 13:57:43 -0300

    prototype

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -2,3 +2,15 @@ function Blockchain() {
     this.chain = [];
     this.newTransactions = [];
 }
+
+Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash,hash) {
+    const newBlock = {
+        index: this.chain.length + 1,
+        timestamp: Date.now(),
+        transactions: this.newTransactions,
+        nonce: nonce, 
+        hash: hash,
+        previousBlockHash: previousBlockHash
+    };
+
+}
