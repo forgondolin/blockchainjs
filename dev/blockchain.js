ecb51a090111efc62975e28d0e6ddc4e2fd52aba
kaleb <kaleb.lucas@aluno.ufabc.edu.br>
2019-12-31 14:18:55 -0300

    metodo de teste

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -13,4 +13,15 @@ Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash,hash) {
         previousBlockHash: previousBlockHash
     };
 
+    this.newTransactions = [];
+    this.chain.push(newBlock);
+
+    return newBlock;
 }
+
+
+Blockchain.prototype.getLastBlock = function() {
+    return this.chain[this.chain.length - 1];
+}
+
+module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -1,1 +1,9 @@
-ECHO est  ativado.
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain;
+
+bitcoin.createNewBlock(2349,'aydya78dga87dg8ads','1y87137y173y11913');
+bitcoin.createNewBlock(111,'AHUDASUDHAUSHD','A9S89YSAHDAH');
+bitcoin.createNewBlock(2899,'HAUDHASDHA8888A','U8UZ89UZY89YWY');
+
+console.log(bitcoin);
