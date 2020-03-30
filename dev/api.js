7bd8feb23f8a0f1547952e3ce4c249cfc3ec048b
Kaleb <tr707962@oi.corp.net>
2020-03-30 17:41:40 -0300

    get req for bc

--- dev/api.js
+++ dev/api.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var app = express();
 const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.get('/blockchain', function(req,res){
-   
+   res.send(bitcoin);
 });
 
 app.post('/transaction', function(req,res){
-
+        console.log(req.body);
+        res.send(`O total de grana é ${req.body.ammount}`);
 });
 
 app.get('/mine', function(req,res){
