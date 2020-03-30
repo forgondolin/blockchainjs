79c91e465298d5e4e4ec5eaba3122b8799c0acb4
Kaleb <tr707962@oi.corp.net>
2020-03-30 17:25:28 -0300

    setting api and routes

---
+++ dev/api.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express()
+
+app.get('/blockchain', function(req,res){
+   
+})
+
+app.post('/transaction', function(req,res){
+
+})
+
+app.get('/mine', function(req,res){
+
+})
+
+app.listen(3000, function(){
+    console.log('listening port 3000');
+});
