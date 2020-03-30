b5a842052b5af2e88ed9fdb25eec6524c6c793c5
Kaleb <tr707962@oi.corp.net>
2020-03-30 17:35:58 -0300

    bodyparser for data

--- dev/api.js
+++ dev/api.js
@@ -1,17 +1,21 @@
 var express = require('express');
-var app = express()
+var app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.get('/blockchain', function(req,res){
    
-})
+});
 
 app.post('/transaction', function(req,res){
 
-})
+});
 
 app.get('/mine', function(req,res){
 
-})
+});
 
 app.listen(3000, function(){
     console.log('listening port 3000');
