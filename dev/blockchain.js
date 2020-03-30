dbde5d92e572ba988c1ac277cdcf2163477c6cc7
Kaleb <tr707962@oi.corp.net>
2020-03-30 18:31:56 -0300

    new nodes for api

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,8 +1,12 @@
 const sha256 = require('sha256');
+const currentNodeUrl = process.argv[3];
 
 function Blockchain() {
     this.chain = [];
     this.pendingTransactions = [];
+    
+    this.currentNodeUrl = currentNodeUrl;
+    this.networkNodes = [];
 
     this.createNewBlock(100,'0','0');
 }
